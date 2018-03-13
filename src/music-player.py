feeecd629e97729bc0644b837291c36a887fb151
David Dugue <ddugue@kumoweb.ca>
2018-03-12 21:42:16 -0400

    Fixed scanning method

--- src/music-player.py
+++ src/music-player.py
@@ -1,6 +1,6 @@
 
-from flask import Flask
-from models import db, Genre
+from flask import Flask, jsonify
+from models import db, Genre, Album, Track
 
 app = Flask(__name__)
 app.config.from_json('../etc/config.json')
@@ -9,10 +9,9 @@ db.init_app(app)
 @app.route('/')
 def hello_world():
     genres = list(Genre.query.all())
+    albums = list(Album.query.all())
 
-    for g in genres:
-        print(g)
-    return ",".join(map(repr,genres))
+    return jsonify(albums)
 
 #-- CLI Commands
 import click
@@ -29,18 +28,31 @@ def scan():
     click.echo('Initializing the db...')
     db.create_all()
 
-    click.echo('Installing fixtures...')
-    acid = Genre(name="Acid Jazz")
-    db.session.add(acid)
-    db.session.commit()
+    # click.echo('Installing fixtures...')
+    # acid = Genre(name="Acid Jazz")
+    # db.session.add(acid)
+    # db.session.commit()
 
     click.echo('Scanning music library...')
     for dirpath, dirs, filenames in os.walk(app.config['MUSIC_LIBRARY']):
         for f in filenames:
+            track_info = metadata.get_track_info(dirpath, f)
+            if not album:
+                album, created = Album.get_or_create(**track_info)
+                if created:
+                    click.echo('Adding album {0.name} by {0.artist.name}...'.format(album))
+
+            if track_info:
+                track_info['album'] = album
+                track, created = Track.get_or_create(**track_info)
+                if created:
+                    click.echo('Adding track {0.title} from {0.album.name}...'.format(track))
+        album = None
+        db.session.commit()
+
             # meta = EasyID3(os.path.join(dirpath, f))
             # if meta:
             #     print(meta.keys())
-            print(metadata.get_track_info(dirpath, f))
             # if m:
             #     print(m.info)
             #     print(m.tags.keys())
