39111838ff7c969c49f1089909861fea8788f32b
David Dugue <ddugue@kumoweb.ca>
2018-03-18 22:01:14 -0400

    Added a few icons

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 const svgIcons = {};
 svgIcons.cassette = require('../img/cassette.svg');
+svgIcons.rockon = require('../img/rockon.svg');
+svgIcons.vinyl = require('../img/vinyl.svg');
+svgIcons.musicNote = require('../img/music_note.svg');
 
 const Icon = ({ name, ...rest }) => (
   <div className="svg-icon" {...rest} dangerouslySetInnerHTML={{ __html: svgIcons[name] }} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,23 @@ import Icon from '../components/Icon';
 const Navbar = (props) => {
   return (
     <nav>
-      <NavButton text="Hearth" className="brown" to="/" icon={<Icon name="cassette" />} />
+      <NavButton text="Hearth" className="red" to="/" icon={<Icon name="cassette" />} />
       <div className="gap" />
-      <NavButton text="Albums" className="red" to="/albums" icon={<Icon name="cassette" />} />
-      <NavButton text="Artists" className="red" to="/artists" icon={<Icon name="cassette" />} />
-      <NavButton text="Songs" className="red" to="/songs" icon={<Icon name="cassette" />} />
+      <NavButton text="Albums" className="brown" to="/albums" icon={<Icon name="vinyl" />} />
+      <NavButton text="Artists" className="brown" to="/artists" icon={<Icon name="rockon" />} />
+      <NavButton text="Songs" className="brown" to="/songs" icon={<Icon name="musicNote" />} />
+      <div className="gap" />
+      <NavButton text="Genres" className="brown" to="/genre" icon={<Icon name="cassette" />} />
+      <NavButton text="Mixes" className="brown" to="/mixes" icon={<Icon name="cassette" />} />
+      <NavButton text="Queue" className="brown" to="/queue" icon={<Icon name="cassette" />} />
+      <div className="gap" />
+      <NavButton text="Output" className="brown" to="/output" icon={<Icon name="cassette" />} />
+      <NavButton text="Account" className="brown" to="/account" icon={<Icon name="cassette" />} />
+      <div className="gap" />
+      <NavButton text="Help" className="brown" to="/help" icon={<Icon name="cassette" />} />
+      <NavButton text="Info" className="brown" to="/info" icon={<Icon name="cassette" />} />
       <div className="fill" />
-      <NavButton text="Search" className="brown" to="/search" icon={<Icon name="cassette" />} />
+      <NavButton text="Search" className="red" to="/search" icon={<Icon name="cassette" />} />
     </nav>
   )
 }
