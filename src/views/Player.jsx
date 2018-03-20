969d7edb92077f9e02f6d2903efd37acd6f006c7
David Dugue <ddugue@kumoweb.ca>
2018-03-19 20:56:26 -0400

    Improved cassette player

--- src/views/Player.jsx
+++ src/views/Player.jsx
@@ -4,9 +4,12 @@ export default class Player extends React.Component {
   render() {
     return (
       <div className="music-player">
+        <div className="line" />
         <div className="cassette">
           <h2>Title of the long song</h2>
           <div className="inner">
+            <div className="wheel" style={{borderWidth: 50}} />
+            <div className="wheel" style={{borderWidth: 10}}/>
           </div>
         </div>
       </div>
